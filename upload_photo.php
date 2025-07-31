fc2732b72bf8c688a17421995ab2ae85555d34c4
David Aleksic <153184512+David05Aleksic@users.noreply.github.com>
2025-07-31 14:42:37 +0200

    Update upload_photo.php

--- upload_photo.php
+++ upload_photo.php
@@ -1,14 +1,14 @@
 <?php 
 
-    $photo = $_FILES['photo']; // ovaj naziv photo je onaj sto pise u js
+    $photo = $_FILES['photo']; 
 
-    $photo_name = basename($photo['name']); // samo daje ime slike a ne putanju do slike
+    $photo_name = basename($photo['name']); 
 
-    $photo_path = 'member_photos/' . $photo_name; // cuvamo sliku korisnika u folder
+    $photo_path = 'member_photos/' . $photo_name; 
 
     $allowed_ext = ['jpg', 'jpeg', 'png', 'gif'];
 
-    $ext = pathinfo($photo_name, PATHINFO_EXTENSION); // ako se ova ekstenzija nalazi u nizu dozvoljena je
+    $ext = pathinfo($photo_name, PATHINFO_EXTENSION);
 
     if(in_array($ext, $allowed_ext) && $photo['size'] < 2000000) {
         move_uploaded_file($photo['tmp_name'], $photo_path);
