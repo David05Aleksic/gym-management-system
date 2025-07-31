56f5e1e4df58dec32aaf1e1343fd15d9f97ba833
David Aleksic <153184512+David05Aleksic@users.noreply.github.com>
2025-07-31 15:18:30 +0200

    Update assign_trainer.php

--- assign_trainer.php
+++ assign_trainer.php
@@ -2,21 +2,25 @@
 
     require_once 'config.php';
 
-    if($_SERVER['REQUEST_METHOD'] == "POST") {
-        $member_id = intval($_POST['member_id']);
-        $trainer_id = intval($_POST['trainer_id']);
-
-        $sql = "UPDATE members SET trainer_id = ? WHERE member_id = ?";
-
-        $execute = $con->prepare($sql);
-        $execute->bind_param("ii", $trainer_id, $member_id);
-
-        $execute->execute();
-
-        $_SESSION['success_message'] = "Trainer is successfully assigned to Member!";
-        header("location: admin_dashboard.php");
+    if (!isset($_SESSION['admin_id'])) {
+        header("Location: index.php");
         exit();
-
     }
 
-?>
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $member_id = intval($_POST['member']);
+        $trainer_id = intval($_POST['trainer']);
+
+        $sql = "UPDATE members SET trainer_id = ? WHERE member_id = ?";
+        $stmt = $con->prepare($sql);
+        $stmt->bind_param("ii", $trainer_id, $member_id);
+
+        if ($stmt->execute()) {
+            $_SESSION['success_message'] = "Trainer is successfully assigned to Member!";
+        } else {
+            $_SESSION['success_message'] = "Failed to assign trainer: " . $stmt->error;
+        }
+
+        header("Location: admin_dashboard.php");
+        exit();
+}
